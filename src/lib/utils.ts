8c941c00b6ec558d8fda12c50c83aceb9194b12d
Anudeep Chowdary <sirigirianudeep348@gmail.com>
2024-05-28 13:13:48 +0530

    added metadata

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,6 @@
 import { CURRENCY_CONVERTER } from "@/config/products";
 import { type ClassValue, clsx } from "clsx";
+import { Metadata } from "next";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -17,3 +18,34 @@ export const formatPrice = (price: number, convertCurrency?: boolean) => {
   }
   return formatter.format(price * CURRENCY_CONVERTER);
 };
+
+export function constructMetaData({
+  title = "CaseCobra - custom high-quality phone cases",
+  description = "Create custom high-quality phone cases in seconds",
+  image = "/thumbnail.png",
+  icons = "/favicon.ico",
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [{ url: image }],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      creator: "@anudeepsirigir1",
+    },
+    icons,
+    metadataBase: new URL("https://casecobra-iota.vercel.app/"),
+  };
+}
