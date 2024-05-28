5aab91a87c9161eacde0125ebc3b20b9ac30e46b
Anudeep Chowdary <sirigirianudeep348@gmail.com>
2024-05-28 08:40:52 +0530

    Added email service to send a thankyou email to user on successful purchase

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -3,6 +3,10 @@ import { stripe } from "@/lib/stripe";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
+import { Resend } from "resend";
+import OrderRecivedEmail from "@/components/email/OrderRecivedEmail";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: Request) {
   try {
@@ -38,7 +42,7 @@ export async function POST(req: Request) {
       const billingAddress = session.customer_details!.address;
       const shippingAddress = session.customer_details!.address;
 
-      await db.order.update({
+      const updateOrder = await db.order.update({
         where: {
           id: orderId,
         },
@@ -66,6 +70,25 @@ export async function POST(req: Request) {
             },
           },
         },
+      });
+
+      await resend.emails.send({
+        from: "CaseCobra <180040344ece@gmail.com>",
+        to: [event.data.object.customer_details.email],
+        subject: "Thanks for your Order!",
+        react: OrderRecivedEmail({
+          orderId,
+          orderDate: updateOrder.createdAt.toLocaleDateString(),
+          // @ts-ignore
+          shippingAddress: {
+            name: session.customer_details!.name!,
+            city: shippingAddress!.city!,
+            country: shippingAddress!.country!,
+            postalCode: shippingAddress!.postal_code!,
+            street: shippingAddress!.line1!,
+            state: shippingAddress!.state!,
+          },
+        }),
       });
     }
 
