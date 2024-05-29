8d21d5cb2f781cf9c12114ee4ba22a241959205e
Anudeep Chowdary <sirigirianudeep348@gmail.com>
2024-05-29 15:33:44 +0530

    fixed styling issues on mobile screen

--- src/app/myorders/page.tsx
+++ src/app/myorders/page.tsx
@@ -30,20 +30,29 @@ const Page = () => {
         Your Orders
       </h3>
       {data?.map((order) => (
-        <Card key={order.id} className="mb-5 p-4 sm:text-lg md:text-lg">
-          <CardHeader className="space-y-0 flex flex-row items-center ">
-            <CardTitle className="mr-2.5">Oder ID: </CardTitle>
-            <CardDescription className="pt-0">{order.id}</CardDescription>
+        <Card
+          key={order.id}
+          className="mb-8 p-4 text-xs sm:text-sm md:text-lg lg:text-xl"
+        >
+          <CardHeader className="space-y-0 flex flex-col sm:flex-row sm:items-center ">
+            <CardTitle className="mr-0 sm:mr-2.5">Oder ID: </CardTitle>
+            <CardDescription className="pt-2 sm:pt-0 md:text-lg">
+              {order.id}
+            </CardDescription>
           </CardHeader>
 
           <div className="h-[1px] w-full bg-gray-300" />
-          <CardContent className="grid grid-cols-1 md:grid-cols-3 gap-5 md:mt-8">
-            <div className="p-16 md:p-0 xl:p-10">
-              <Phone imgSrc={order.configuration.croppedImageUrl!} />
+
+          <CardContent className="grid grid-cols-1 md:grid-cols-3 md:gap-5 md:mt-8">
+            <div className="p-10 md:p-0 xl:p-10">
+              <Phone
+                className="rounded-md overflow-hidden"
+                imgSrc={order.configuration.croppedImageUrl!}
+              />
             </div>
 
-            <div className=" grid col-span-2 xl:mt-5 ">
-              <div className="m-3 p-3">
+            <div className=" grid col-span-2 xl:mt-5">
+              <div className="m-0 p-0 sm:m-3 sm:p-3 ">
                 <h3 className="font-semibold  mb-1">Order Details</h3>
 
                 <div className="h-[1px] w-full bg-gray-300 mb-4" />
@@ -51,31 +60,35 @@ const Page = () => {
                 <div className="flex flex-col gap-3">
                   <div className="flex justify-between">
                     <span className="font-medium">Color: </span>
-                    <p>{order.configuration.color}</p>
+                    <p className="text-gray-600">{order.configuration.color}</p>
                   </div>
 
                   <div className="flex justify-between">
                     <span className="font-medium">Model: </span>
-                    <p>{order.configuration.model}</p>
+                    <p className="text-gray-600">{order.configuration.model}</p>
                   </div>
 
                   <div className="flex justify-between">
                     <span className="font-medium">Material: </span>
-                    <p>{order.configuration.material}</p>
+                    <p className="text-gray-600">
+                      {order.configuration.material}
+                    </p>
                   </div>
 
                   <div className="flex justify-between">
                     <span className="font-medium">Finish: </span>
-                    <p>{order.configuration.finish}</p>
+                    <p className="text-gray-600">
+                      {order.configuration.finish}
+                    </p>
                   </div>
 
-                  <div className="hidden xl:flex justify-between mt-2">
+                  <div className="hidden xl:flex xl:justify-between mt-4 gap-16">
                     <span className="font-medium">Shipping Address: </span>
-                    <div className="text-base">
-                      <p>{order.shippingAddress?.street} </p>
-                      <p>{order.shippingAddress?.state} </p>
-                      <p>{order.shippingAddress?.country} </p>
-                      <p>{order.shippingAddress?.postalCode}</p>
+                    <div className="text-base mx-auto text-gray-600 flex flex-col">
+                      {`${order.shippingAddress?.street} , 
+                      ${order.shippingAddress?.state} , 
+                      ${order.shippingAddress?.country} ,
+                      ${order.shippingAddress?.postalCode}.`}
                     </div>
                   </div>
                 </div>
@@ -85,14 +98,16 @@ const Page = () => {
 
           <div className="h-[1px] w-full bg-gray-300" />
 
-          <CardFooter className="flex flex-col mt-5 gap-3">
-            <div className=" w-full flex justify-between">
+          <CardFooter className="flex flex-col mt-5 gap-5  sm:gap-3">
+            <div className=" w-full flex flex-col sm:flex-row gap-1 justify-start sm:justify-between">
               <p className="font-medium ">Current Status:</p>
-              {order.status}
+              <span className="text-gray-600">{order.status}</span>
             </div>
-            <div className="w-full flex justify-between">
+            <div className="w-full flex flex-col gap-1 sm:flex-row justify-between">
               <p className="font-medium">Amount Paid:</p>
-              {formatPrice(order.amount, false)}
+              <span className="text-gray-600">
+                {formatPrice(order.amount, false)}
+              </span>
             </div>
           </CardFooter>
         </Card>
