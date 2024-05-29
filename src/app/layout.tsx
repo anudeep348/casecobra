d9b1f04f4fb15d09d2d1b0597e56003cd8f57958
Anudeep Chowdary <sirigirianudeep348@gmail.com>
2024-05-29 15:42:21 +0530

    added vercel spped insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Footer from "@/components/Footer";
 import { Toaster } from "@/components/ui/toaster";
 import Providers from "@/components/Providers";
 import { constructMetaData } from "@/lib/utils";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const recursive = Recursive({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
         <main className="flex flex-col min-h-[calc(100vh-3.5rem-1px)] grainy-light">
           <div className="flex-1 flex flex-col h-full">
             <Providers>{children}</Providers>
+            <SpeedInsights />
           </div>
           <Footer />
         </main>
