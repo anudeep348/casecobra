eeb922396208ca947fde6c9b17b5b4a7b3742e07
Anudeep Chowdary <sirigirianudeep348@gmail.com>
2024-11-06 23:57:59 +0530

    fixed overflow issue in reviews component

--- src/components/Phone.tsx
+++ src/components/Phone.tsx
@@ -29,7 +29,7 @@ const Phone = ({ imgSrc, className, dark = false, ...props }: PhoneProps) => {
 
       <div className="absolute -z-10 inset-0">
         <Img
-          className="object-cover min-w-full min-h-full"
+          className="min-w-full min-h-full"
           src={imgSrc}
           alt="overlaying phone image"
         />
