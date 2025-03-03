99eed53427d41e9cefc068f256f0373b7869f8e2
Anudeep Chowdary <sirigirianudeep348@gmail.com>
2025-03-03 15:55:29 +0530

    added another uploadthing domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["utfs.io", "utfs.sh", "ufs.sh"],
+    domains: ["utfs.io", "utfs.sh", "ufs.sh", "q9qcsj56u3.ufs.sh"],
   },
 };
 
